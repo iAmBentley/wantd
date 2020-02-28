444274bad791111c5383395cfd038fe7cc414037
iAmBentley <jlb.bentley@gmail.com>
2020-02-27 23:15:42 -0500

    Update dependancies and add browsersync

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
 const mix = require('laravel-mix');
+const domain = 'wantd.test';
+const homedir = require('os').homedir();
 
 require('laravel-mix-tailwind');
 require('laravel-mix-purgecss');
@@ -16,7 +18,16 @@ require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
    .postCss('resources/css/app.css', 'public/css')
-   .tailwind('./tailwind.config.js');
+   .tailwind('./tailwind.config.js')
+   .browserSync({
+	   proxy: 'https://' + domain,
+	   host: domain,
+	   open: 'external',
+	   https: {
+		   key: homedir + '/.config/valet/Certificates/' + domain + '.key',
+		   cert: homedir + '/.config/valet/Certificates/' + domain + '.crt',
+	   }
+	});
 
 if (mix.inProduction()) {
   mix
