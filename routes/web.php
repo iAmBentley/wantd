8074c20285b7eb137a11a040397d571429b93465
iAmBentley <joey@moticorp.com>
2020-02-28 12:10:18 -0500

    Add dashboard and redirect to it at login + add user seeder

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
-
+Route::middleware(['web'])->group(function () {
+	// DASHBOARD ROUTE
+	Route::get('dashboard', 'DashboardController@index')->name('dashboard');
+});
