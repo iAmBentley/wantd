804425a0509ad65f41cc2fd8004a21ed3fbe2347
iAmBentley <jlb.bentley@gmail.com>
2020-03-08 23:48:26 -0400

    add item-delete-modal on items.index

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -295,11 +295,11 @@
 													</div>
 													{{-- Delete Button --}}
 													<div class="pb-3">
-														<a href="#" class="p-4 hover:bg-gray-600 rounded">
+														<button @click="showDeleteModal = true" type="button" class="p-4 hover:bg-gray-600 rounded">
 															<svg width="36" height="36" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
 																<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7L18.1327 19.1425C18.0579 20.1891 17.187 21 16.1378 21H7.86224C6.81296 21 5.94208 20.1891 5.86732 19.1425L5 7M10 11V17M14 11V17M15 7V4C15 3.44772 14.5523 3 14 3H10C9.44772 3 9 3.44772 9 4V7M4 7H20"/>
 															</svg>
-														</a>
+														</button>
 													</div>
 												</div>
 											</div>
@@ -506,4 +506,5 @@
 			</div>
 		</div>
 	</main>
+	@include('layouts.modals.delete-item')
 @endsection
